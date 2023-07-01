d7e8a38afc8820df7692d9937d65fe9e968c0ac9
adibfuego90 <adiburraman11@gmail.com>
2023-07-01 07:40:39 +0600

    array new feature-02

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ const reversedArray = arr.toReversed();
 console.log(reversedArray);
 console.log(arr); //main array is not muted because of using to.Reversed method
  */
+
+/* -----------------*----------------------------- */
+/* 
+// 02 feature
+// original array
+const arr = [1, 2, 3, 4, 5, 6];
+
+// splice method use to remove or add new element to the array
+// const removedEl = arr.splice(2, 2, 32, 33);
+// console.log(removedEl);
+// console.log(arr);  //array are muted using splice method 
+
+// new feature
+const newArray = arr.toSpliced(2, 1, 33, 34); //not mute array
+console.log(newArray);
+console.log(arr);
+ */

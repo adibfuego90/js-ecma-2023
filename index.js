6e62e2828f3a78bfe0ecd322478a02815bddf28c
adibfuego90 <adiburraman11@gmail.com>
2023-07-01 07:07:59 +0600

    array new feature-01

---
+++ index.js
@@ -0,0 +1,12 @@
+/* //original array 1 : method 
+const arr = [12, 23, 34, 345, 23];
+
+// Muted reverse array
+// const reversedArray = arr.reverse();
+
+// Unmuted (es-14)
+const reversedArray = arr.toReversed();
+
+console.log(reversedArray);
+console.log(arr); //main array is not muted because of using to.Reversed method
+ */
